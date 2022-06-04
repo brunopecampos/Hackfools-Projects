350194f9505f7d688d17b62ecf6b1973a34357e9
Dudis <eduardolopes.2780@gmail.com>
2022-06-04 14:58:37 -0300

    Adds DrinkSelector.

---
+++ speedroni/src/DrinkSelector.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+
+
+let correct=Math.floor(Math.random() * 15);
+
+function DrinkSelector(props){
+    
+    
+    const img = props.href;
+    let buttons=[];
+    const [colors, setColors] = useState(Array(15).fill('black'));
+    const clicked = (i)=>{
+        
+        
+        let newcolors=[...colors];
+        if (i===correct){            
+            newcolors[i]='green'     
+            props.setCorrect(correct)       
+        }
+        else{
+            newcolors[i]='red'
+        }
+        setColors(newcolors)
+    }
+    
+    buttons = colors.map(
+        (el, i) =>{
+            return <button onClick={() => clicked(i)} style={{border: '5px solid ' + el, margin:"5px"}}> <span><img style={{height: "150px"}} src={img}></img></span></button>
+        }
+    )
+    const style={
+        display: "flex",
+        flexWrap: "wrap"
+    }   
+    return <div className="drinkSelector" style={style}>{buttons}</div>
+}
+
+export default DrinkSelector;
